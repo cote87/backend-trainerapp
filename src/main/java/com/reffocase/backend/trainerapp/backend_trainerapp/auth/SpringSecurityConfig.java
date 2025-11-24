eea7b81ba9e7cb1fd7ba6f44a65427ca89449084
José Luis Leiva <cote.jll@gmail.com>
2025-11-24 13:51:24 -0300

    Actualización desde servidor - backend

--- src/main/java/com/reffocase/backend/trainerapp/backend_trainerapp/auth/SpringSecurityConfig.java
+++ src/main/java/com/reffocase/backend/trainerapp/backend_trainerapp/auth/SpringSecurityConfig.java
@@ -105,8 +105,10 @@ public class SpringSecurityConfig {
     @Bean
     CorsConfigurationSource corsConfigurationSource() {
         CorsConfiguration config = new CorsConfiguration();
-        config.setAllowedOrigins(List.of("http://localhost:5173"));
-        config.setAllowedMethods(List.of("GET", "POST", "PUT", "DELETE"));
+        config.setAllowedOrigins(List.of(
+		"http:/redfocase.com.ar",
+		"http://localhost:5173"));
+	config.setAllowedMethods(List.of("GET", "POST", "PUT", "DELETE", "OPTIONS"));
         config.setAllowedHeaders(List.of("Authorization", "Content-Type"));
         config.setAllowCredentials(true);
 
